79cb6c069a6fcd8491b90132c2aa92d9dac6ad18
HoHenHeimpepsi <diegonicolas2548@gmail.com>
2023-11-04 22:51:20 -0400

    uwu

--- models/user.js
+++ models/user.js
@@ -1,2 +1,18 @@
 import mongoose from "mongoose";
 
+const userSchema = mongoose.Schema({
+    Name: {
+        type: String,
+        required: true,
+      },
+      Rut:{
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    });
+
+export default mongoose.model("User", userSchema);
